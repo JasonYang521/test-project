ff2dadae64608b6e853e5212b623dedff4e862e7
Jason Yang <mr.jasonyang@gmail.com>
2014-08-06 22:08:55 -0700

    Commit of test project

---
+++ MobilizeMe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobilizeMe.MvcApplication" Language="C#" %>
---
+++ MobilizeMe/Scripts/customescripts.js
@@ -0,0 +1,7 @@
+﻿$(function(){
+    $('#alertMe').click(function (e) {
+        e.preventDefault();
+        $('#successAlert').slideDown();
+    });
+
+});
